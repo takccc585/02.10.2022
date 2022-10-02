929fe302948da46394320e24204b2847f5a6402d
takccc585 <114169693+takccc585@users.noreply.github.com>
2022-10-02 19:28:04 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,131 @@
+#include<iostream>
+using namespace std;
+int main() {
+	//15
+	/*int a, b,c;
+	cin >> a >> b >> c;
+	if (a > c and b > c) {
+		int r = a + b;
+		cout << r << endl;
+	}
+	if ( b > a and c > a) {
+		int r = c + b;
+		cout << r << endl;
+	}
+	if ( a > b and c > b) {
+		int r = a + c;
+		cout << r << endl;
+	}*/
+	
+	//16
+	/*float a, b,c;
+	cin >> a >> b >> c;
+	if (a < b and b < c) {
+		a = a * 2;
+		b = a * 2;
+		c = c * 2;
+	}
+	else {
+		a = -a;
+		b = -b;
+		c = -c;
+	}
+	cout << " " << a << " " << b << " " << c;*/
+
+	//17
+	/*float a, b,c;
+	cin >> a >> b >> c;
+	if ((a < b and b < c) or (a > b and b > c)) {
+		a = a * 2;
+		b = b * 2;
+		c = c * 2;
+	}
+	else {
+		a = -a;
+		b = -b;
+		c = -c;
+	}
+	cout << " " << a << " " << b << " " << c;*/
+
+	//18
+	/*int a, b, c;
+	cin >> a >> b >> c;
+	if ((a < b and b == c) or (a > b and b == c)) {
+		cout << "1";
+	}
+	if ((b < a and a == c) or (b > a and a == c)) {
+		cout << "2";
+	}
+	if ((c < a and a == b) or (c > a and a == b)) {
+		cout << "3";
+	}*/
+
+	//19
+	/*int a, b, c, h;
+	cin >> a >> b >> c >> h;
+	if (a !=b and a!=c and a!=h ) {
+		cout << "1";
+	}
+	if (h != a and h != c and b != h) {
+		cout << "2";
+	}
+	if (b != h and b != c and a != b) {
+		cout << "3";
+	}
+	if (c != b and c != a and c != h) {
+		cout << "4";
+	}*/
+
+	//20
+	/*int a, b, c;
+	cin >> a >> b >> c;
+	int r1 = abs(a - b);
+	int r2 = abs(a - c);
+	if (r1<r2) {
+		cout << "1";
+	}
+	if (r2<r1) {
+		cout << "2";
+	}*/
+	
+	//21
+	/*int a, b;
+	cin >> a >> b ;
+	if (a==0 and b==0) {
+		cout << "0";
+	}
+	if (a==0 and b!=0) {
+		cout << "1";
+	}
+	if (a!=0 and b==0) {
+		cout << "2";
+	}
+	if (a != 0 and b != 0) {
+		cout << "3";
+	}*/
+
+	//22
+	/*int a, b;
+	cin >> a >> b;
+	if (a>0 and b>0) {
+		cout << "1";
+	}
+	if (a < 0 and b > 0) {
+		cout << "2";
+	}
+	if (a < 0 and b < 0) {
+		cout << "3";
+	}
+	if (a > 0 and b < 0) {
+		cout << "4";
+	}*/
+	
+	//23
+	int x1, y1, y2, x2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	if () {
+		
+	}
+
+	return 0;
+}
